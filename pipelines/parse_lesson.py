32b14696335bee0c0b91487e75fc32ab0d603cb1
Daiwery <andrey.luk10@gmail.com>
2022-03-06 16:18:18 +0300

    Refactoring

--- pipelines/parse_lesson.py
+++ pipelines/parse_lesson.py
@@ -3,20 +3,17 @@ import pandas as pd
 import re
 
 
-def parse_name_lesson(url):
+def parse_lesson(url):
     engine = create_engine(url)
 
-    lessons = pd.read_sql("select * from lessons", engine)
-    lessons = pd.DataFrame(lessons, columns=lessons.columns)
-
-    decode_patterns = pd.read_sql("select * from decode_patterns", engine)
-    decode_patterns = pd.DataFrame(decode_patterns, columns=decode_patterns.columns)
+    lessons = pd.read_sql_table("lessons", engine)
+    decode_patterns = pd.read_sql_table("decode_patterns", engine)
     decode_patterns.sort_values("priority")
 
     parsed_names = []
     for index, row in lessons.iterrows():
         name = row["name"]
-        parsed_name = {"subject": "", "teacher": "", "place": ""}
+        parsed_name = None
 
         for regex in decode_patterns["regex"]:
             results = re.match(regex, name)
@@ -29,5 +26,5 @@ def parse_name_lesson(url):
 
     lessons = pd.concat([lessons, pd.DataFrame(parsed_names)], axis=1)
     with engine.begin() as connection:
-        lessons.to_sql("parsed_lessons", con=connection, if_exists='replace')
+        lessons.to_sql("parsed_lessons_temporary_table", con=connection, if_exists='replace')
 
---
+++ pipelines/parse_timetable.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+import timetable.timetable_parser as parser
+
+
+def parse_timetable(url):
+    USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
+    HEADERS = {"User-Agent": USER_AGENT}
+
+    results = pd.DataFrame()
+    sources = [[1, 2, 1]]  # [[1, 1, 6], [1, 2, 6], [1, 3, 6], [2, 1, 6], [2, 2, 6], [2, 3, 6], [3, 1, 10], [3, 2, 8],
+    # [4, 1, 10], [4, 2, 8], [5, 1, 13], [5, 2, 10], [6, 1, 10], [6, 2, 9]]
+    for source in sources:
+        for group in range(1, source[2]+1):
+            html = requests.get('http://ras.phys.msu.ru/table/{year}/{stream}/{group}.htm'
+                                .format(year=source[0], stream=source[1], group=group), headers=HEADERS).content
+            results = pd.concat([results, pd.DataFrame(parser.run(html))])
+
+    engine = create_engine(url)
+    with engine.begin() as connection:
+        results.to_sql("lessons", con=connection, if_exists='replace')
