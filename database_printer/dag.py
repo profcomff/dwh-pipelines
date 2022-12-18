10ac97f5bef52f371783c73c2d629819c72e4cfb
SergeyZamyatin1 <104686457+SergeyZamyatin1@users.noreply.github.com>
2022-12-18 10:48:55 +0300

    csrftoken

--- database_printer/dag.py
+++ database_printer/dag.py
@@ -3,5 +3,10 @@ from bs4 import BeautifulSoup
 
 response = requests.get("https://lk.msuprof.com/adminlogin/?next=/admin")
 
+string = response.headers['Set-Cookie']
+pattern = r'csrftoken=\S*;'
+string = re.findall(pattern, string)[0]
+csrftoken = string.split(sep = "=")[1][:-1]
+
 html = BeautifulSoup(response.text, "html.parser")
 middle_token = html.find_all("input", {"name": "csrfmiddlewaretoken"})[0].attrs["value"]
