66bfac46a9722df6cd04c5067d46a4d409e96670
Daiwery <andrey.luk10@gmail.com>
2022-12-25 10:46:15 +0300

    Database

--- database_printer/dag.py
+++ database_printer/dag.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 import requests
@@ -17,9 +18,18 @@ html = BeautifulSoup(response.text, "html.parser")
 middle_token = html.find_all("input", {"name": "csrfmiddlewaretoken"})[0].attrs["value"]
 
 # ---------- Login ----------
-json = {"username": password.login, "password": password.password,
+data = {"username": password.login, "password": password.password,
         "next": "/admin", "csrfmiddlewaretoken": middle_token}
 headers = {"cookie": f"csrftoken={csrftoken}", "referer": "https://lk.msuprof.com/adminlogin/?next=/admin"}
 
-response = requests.post("https://lk.msuprof.com/adminlogin/?next=/admin", data=json, headers=headers).request
-print(response.headers["Cookie"])
+response = requests.post("https://lk.msuprof.com/adminlogin/?next=/admin", data=data, headers=headers).request
+csrftoken = re.findall(r"csrftoken=(\S*);", response.headers["Cookie"])[0]
+sessionid = re.findall(r"sessionid=(\S*)", response.headers["Cookie"])[0]
+
+# ---------- Get Table ----------
+headers = {"cookie": f"csrftoken={csrftoken}; sessionid={sessionid}"}
+data = {"current-role": "Администратор", "f_role": "", "f_status": "", "page-status": "user"}
+
+response = requests.post("https://lk.msuprof.com/get-table/", data=data, headers=headers)
+data = json.loads(response.text)
+
