588835da4f9c92bd168d31551d3fe7c55b9b0745
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2024-05-07 03:09:34 +0300

    Truncate infra logs (#71)
    Удаляем логи, у которых create_ts больше трех дней назад

---
+++ infra/logs.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+
+with DAG(
+    dag_id="truncate_old_infra_logs",
+    start_date=datetime(2024, 1, 1),
+    schedule="0 0 */1 * *",
+    catchup=False,
+    tags=["dwh", "stg", "infra"],
+    default_args={"owner": "dyakovri"},
+):
+    PostgresOperator(
+        postgres_conn_id="postgres_dwh",
+        sql="DELETE FROM \"STG_INFRA\".\"container_log\" "
+            "WHERE create_ts < now() - INTERVAL '3 days';",
+        task_id="execute_delete_statement",
+    )
