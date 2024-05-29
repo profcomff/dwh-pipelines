7fea5f669f76bcb3add05e13a1ca37d04d26559b
SofyaFin <151746034+SofyaFin@users.noreply.github.com>
2024-05-29 18:39:28 +0300

    Update veryrawupdateservicedag.py

--- veryrawupdateservicedag.py
+++ veryrawupdateservicedag.py
@@ -12,34 +12,34 @@ import BeautifulSoup from bs4
 
 @task(task_id='download_pages_to_db', outlets=Dataset("STG_RASPHYSMSU".raw_html)
 def get_from_database_data():
-DB_URI = Connection.get_connection_from_secrets('postgres_dwh').get_uri().replace("postgres://", "postgresql://")
-sql_engine = sa.create_engine(DB_URI)
-with dwh_sql_engine.connect() as conn:
-    event_text = []
-    group_text = []
-    time_interval_text = []
-    data = conn.execute(sa.text(f'''SELECT * FROM "STG_RASPHYSMSU".raw_html''')).fetchall()
-    logging.info("starting parsing")
-    def _parse_data(data):
-        k1 = 0
-        k2 = 0
-        k3 = 0
-        soup = BeautifulSoup(data)
-        for i in soup.find_all('tr',class_=['tditem1','tdsmall']):
-        event_text[k1] = i.get_text()
-        k1+=1
-        for j in soup.find_all('tr', class_='tdtime'):
-            time_interval_text[k2] = j.get_text()
-            k2+=1
-        for u in soup.find_all('b'):
-            sample = re.compile(r"\d{3}")
-            res_middle = u.get_text()
-            group_text[k3] = sample.search(res_middle)
-            k3+=1
-conn.execute(sa.text(f'''delete from ods_timetable_act'''))
-conn.execute(sa.text(f''' CREATE TABLE IF NOT EXISTS ods_timetable_act (url varchar(256) NULL, group_text text NULL, time_interval_text text NULL, event_text text NULL);'''))
-for i in range(len(data)):
-    conn.execute(sa.text(f'''alter table ods_timetable_act insert into (group_text,time_interval_act,time_interval_text) values ({group_text[i],time_interval_text[i],event_text[i]} '''))
+    DB_URI = Connection.get_connection_from_secrets('postgres_dwh').get_uri().replace("postgres://", "postgresql://")
+    sql_engine = sa.create_engine(DB_URI)
+    with dwh_sql_engine.connect() as conn:
+        event_text = []
+        group_text = []
+        time_interval_text = []
+        data = conn.execute(sa.text(f'''SELECT * FROM "STG_RASPHYSMSU".raw_html''')).fetchall()
+        logging.info("starting parsing")
+        def _parse_data(data):
+            k1 = 0
+            k2 = 0
+            k3 = 0
+            soup = BeautifulSoup(data)
+            for i in soup.find_all('tr',class_=['tditem1','tdsmall']):
+            event_text[k1] = i.get_text()
+            k1+=1
+            for j in soup.find_all('tr', class_='tdtime'):
+                time_interval_text[k2] = j.get_text()
+                k2+=1
+            for u in soup.find_all('b'):
+                sample = re.compile(r"\d{3}")
+                res_middle = u.get_text()
+                group_text[k3] = sample.search(res_middle)
+                k3+=1
+    conn.execute(sa.text(f'''delete from ods_timetable_act'''))
+    conn.execute(sa.text(f''' CREATE TABLE IF NOT EXISTS ods_timetable_act (url varchar(256) NULL, group_text text NULL, time_interval_text text NULL, event_text text NULL);'''))
+    for i in range(len(data)):
+        conn.execute(sa.text(f'''alter table ods_timetable_act insert into (group_text,time_interval_act,time_interval_text) values ({group_text[i],time_interval_text[i],event_text[i]} '''))
 
 @dag(
     schedule='0 */1 * * *',
@@ -47,6 +47,9 @@ for i in range(len(data)):
     catchup=False,
     tags=["dwh"],
     default_args={"owner": "dwh", "retries": 3, "retry_delay": timedelta(minutes=5)},
+    def start():
+        get_from_database_data()
+    
 )
 
 
