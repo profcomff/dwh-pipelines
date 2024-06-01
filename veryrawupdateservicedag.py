0d29a939654a8b62785a6da688c8124dffd148fc
SofyaFin <151746034+SofyaFin@users.noreply.github.com>
2024-06-01 08:54:02 +0300

    Update veryrawupdateservicedag.py

--- veryrawupdateservicedag.py
+++ veryrawupdateservicedag.py
@@ -7,14 +7,14 @@ import sqlalchemy as sa
 from airflow.datasets import Dataset
 from airflow.decorators import dag, task
 from airflow.models import Connection, Variable
-import BeautifulSoup from bs4
+from bs4 import BeautifulSoup
 
 
-@task(task_id='download_pages_to_db', outlets=Dataset("STG_RASPHYSMSU".raw_html)
+@task(task_id='download_pages_to_db', outlets=Dataset("STG_RASPHYSMSU".raw_html))
 def get_from_database_data():
     DB_URI = Connection.get_connection_from_secrets('postgres_dwh').get_uri().replace("postgres://", "postgresql://")
     sql_engine = sa.create_engine(DB_URI)
-    with dwh_sql_engine.connect() as conn:
+    with sql_engine.connect() as conn:
         event_text = []
         group_text = []
         time_interval_text = []
@@ -26,8 +26,8 @@ def get_from_database_data():
             k3 = 0
             soup = BeautifulSoup(data)
             for i in soup.find_all('tr',class_=['tditem1','tdsmall']):
-            event_text[k1] = i.get_text()
-            k1+=1
+                event_text[k1] = i.get_text()
+                k1+=1
             for j in soup.find_all('tr', class_='tdtime'):
                 time_interval_text[k2] = j.get_text()
                 k2+=1
@@ -42,15 +42,15 @@ def get_from_database_data():
         conn.execute(sa.text(f'''alter table ods_timetable_act insert into (group_text,time_interval_act,time_interval_text) values ({group_text[i],time_interval_text[i],event_text[i]} '''))
 
 @dag(
-    def start():
-        get_from_database_data()
     schedule='0 */1 * * *',
     start_date=datetime(2024, 1, 1, 2, 0, 0),
     catchup=False,
     tags=["dwh"],
     default_args={"owner": "dwh", "retries": 3, "retry_delay": timedelta(minutes=5)},
-    start()
-    
 )
+def start():
+    get_from_database_data()
+
+start()
 
 
