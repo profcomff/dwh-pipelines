52a17558a880515dd0b76a7c5b6f5ee7b311980a
SofyaFin <151746034+SofyaFin@users.noreply.github.com>
2024-06-21 22:28:53 +0300

    Update veryrawupdateservicedag.py

--- veryrawupdateservicedag.py
+++ veryrawupdateservicedag.py
@@ -21,23 +21,23 @@ def get_from_database_data():
         data = conn.execute(sa.text(f'''SELECT * FROM "STG_RASPHYSMSU".raw_html''')).fetchall()
         logging.info("starting parsing")
         def _parse_data(data):
-            k1 = 0
-            k2 = 0
-            k3 = 0
+            counter_1 = 0
+            counter_2 = 0
+            counter_3 = 0
             soup = BeautifulSoup(data)
             for i in soup.find_all('tr',class_=['tditem1','tdsmall']):
-                event_text[k1] = i.get_text()
-                k1+=1
+                event_text[counter_1] = i.get_text()
+                counter_1+=1
             for j in soup.find_all('tr', class_='tdtime'):
-                time_interval_text[k2] = j.get_text()
-                k2+=1
+                time_interval_text[counter_2] = j.get_text()
+                counter_2+=1
             for u in soup.find_all('b'):
                 sample = re.compile(r"\d{3}")
                 res_middle = u.get_text()
-                group_text[k3] = sample.search(res_middle)
-                k3+=1
-    conn.execute(sa.text(f'''delete from ods_timetable_act'''))
-    conn.execute(sa.text(f''' CREATE TABLE IF NOT EXISTS ods_timetable_act (url varchar(256) NULL, group_text text NULL, time_interval_text text NULL, event_text text NULL);'''))
+                group_text[counter_3] = sample.search(res_middle)
+                conter_3+=1
+    conn.execute(sa.text(f'''delete from ods_timetable_act
+    CREATE TABLE IF NOT EXISTS ods_timetable_act (url varchar(256) NULL, group_text text NULL, time_interval_text text NULL, event_text text NULL);'''))
     for i in range(len(data)):
         conn.execute(sa.text(f'''alter table ods_timetable_act insert into (group_text,time_interval_act,time_interval_text) values ({group_text[i],time_interval_text[i],event_text[i]} '''))
 
