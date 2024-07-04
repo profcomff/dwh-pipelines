39bf564802811381672cb8323a1be2fc188fc412
SofyaFin <151746034+SofyaFin@users.noreply.github.com>
2024-07-04 20:50:55 +0300

    Update ods_to_dm_logs.py

--- infra/ods_to_dm_logs.py
+++ infra/ods_to_dm_logs.py
@@ -17,28 +17,25 @@ def trans():
     with dwh_sql_engine.connect() as conn:
         conn.execute(
             """
-        with sq as (select
-  id,
-  record->>'message' as e_msg,
-  container_name,
-  create_ts
-from
-  "ODS_INFRA_LOGS".container_log
-where
-  record->>'level' = 'ERROR' or record->>'level' = 'CRITICAL') 
-        merge into "DM_INFRA_LOGS".incident_hint as ne 
-        using sq as e on
-        (ne.container_name = e.container_name) and (ne.message = e.e_msg) and (ne.create_ts = e.create_ts)
-        when not matched then 
-        insert (id,msk_record_loaded_dttm,container_name,message,create_ts)
-        values (DEFAULT,now(),e.container_name,e.e_msg,e.create_ts)"""
+    with sq as (select
+    record->>'message' as e_msg,
+    container_name,
+    create_ts
+    from
+    "ODS_INFRA_LOGS".container_log
+    where
+    record->>'level' = 'ERROR' or record->>'level' = 'CRITICAL') 
+    merge into "DM_INFRA_LOGS".incident_hint as ne 
+    using sq as e on
+    (ne.container_name = e.container_name) and (ne.message = e.e_msg) and (ne.create_ts = e.create_ts)
+    when not matched then 
+    insert (id,msk_record_loaded_dttm,container_name,message,create_ts)
+    values (DEFAULT,now(),e.container_name,e.e_msg,e.create_ts)"""
         )
         conn.commit()
 
 
 @dag(
-    schedule="0 */1 * * *",
-    start_date=datetime(2024, 1, 1, 2, 0, 0),
     catchup=False,
     tags=["dwh"],
     default_args={"owner": "dwh", "retries": 3, "retry_delay": timedelta(minutes=5)},
