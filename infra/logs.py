df95039995ea46f09c261d2712c71c20e9bf4f97
SofyaFin <sonyafinutina@gmail.com>
2024-07-28 10:48:38 +0300

    tabulation

--- infra/logs.py
+++ infra/logs.py
@@ -179,20 +179,20 @@ with DAG(
     PostgresOperator(
         postgres_conn_id="postgres_dwh",
         sql=dedent(r"""
-                    with sq as (select
-                    record->>'message' as e_msg,
-                    container_name,
-                    create_ts
-                    from
-                    "ODS_INFRA_LOGS".container_log
-                    where
-                    record->>'level_name' = 'ERROR' or record->>'level_name' = 'CRITICAL') 
+                    with sq as (
+                        select
+                            record->>'message' as e_msg,
+                            container_name,
+                            create_ts
+                        from "ODS_INFRA_LOGS".container_log
+                        where
+                            record->>'level_name' = 'ERROR' or record->>'level_name' = 'CRITICAL') 
                     merge into "DM_INFRA_LOGS".incident_hint as ne 
-                    using sq as e on
-                    (ne.container_name = e.container_name) and (ne.message = e.e_msg) and (ne.create_ts = e.create_ts)
+                    using sq as e 
+                        on (ne.container_name = e.container_name) and (ne.message = e.e_msg) and (ne.create_ts = e.create_ts)
                     when not matched then 
-                    insert (id,msk_record_loaded_dttm,container_name,message,create_ts)
-                    values (DEFAULT,now(),e.container_name,e.e_msg,e.create_ts)"""
+                        insert (id,msk_record_loaded_dttm,container_name,message,create_ts)
+                        values (DEFAULT,now(),e.container_name,e.e_msg,e.create_ts)"""
         ),
         task_id="trans_from_ODS_INFRA_LOGS.container_log_to_DM_INFRA_LOGS.incident_hint_infra_logs",
         inlets=[Dataset("ODS_INFRA_LOGS.container_log")],
