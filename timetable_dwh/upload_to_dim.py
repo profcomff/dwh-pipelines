b2d2744434ad0aa47db003ee5bb99e05bc40f07b
default <parfenov.ma21@physics.msu.ru>
2024-08-18 08:27:00 +0000

    fixes

--- timetable_dwh/upload_to_dim.py
+++ timetable_dwh/upload_to_dim.py
@@ -30,7 +30,7 @@ with DAG(
             select
                     id as group_api_id,
                     number || name as group_name_text,
-                    cast(number as int) as group_number,
+                    number as group_number,
                     'profcomff_timetable_api' as source_name
                 from "STG_TIMETABLE"."group"
             where not is_deleted
