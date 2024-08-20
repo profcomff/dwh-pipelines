af3215ab30e66d5ebd9f54725b61d2b3994c3547
default <finyutina.sa23@physics.msu.ru>
2024-08-20 16:58:41 +0000

    11

--- infra/parse.py
+++ infra/parse.py
@@ -9,18 +9,12 @@ from airflow.decorators import dag, task
 from airflow.models import Connection, Variable
 from bs4 import BeautifulSoup
 
-
-@task(task_id='download_pages_to_db', outlets=Dataset("STG_RASPHYSMSU.raw_html"))
-def get_from_database_data():
-    DB_URI = Connection.get_connection_from_secrets('postgres_dwh').get_uri().replace("postgres://", "postgresql://")
-    sql_engine = sa.create_engine(DB_URI)
-    with sql_engine.connect() as conn:
-        event_text = []
-        group_text = []
-        time_interval_text = []
-        data = conn.execute(sa.text(f'''SELECT * FROM "STG_RASPHYSMSU".raw_html''')).fetchall()
-        logging.info("starting parsing")
-        def _parse_data(data):
+def parse_data(data):
+            event_text = []
+            group_text = []
+            time_interval_text = []
+            final_massive = []
+            counter = 0
             counter_1 = 0
             counter_2 = 0
             counter_3 = 0
@@ -36,10 +30,29 @@ def get_from_database_data():
                 res_middle = u.get_text()
                 group_text[counter_3] = sample.search(res_middle)
                 counter_3+=1
-        conn.execute(sa.text(f'''drop table "ODS_TIMETABLE".ods_timetable_act;
-        CREATE TABLE "ODS_TIMETABLE".ods_timetable_act (url varchar(256) NULL, group_text text NULL, time_interval_text text NULL, event_text text NULL);'''))
-        for i in range(len(data)):
-            conn.execute(sa.text(f'''alter table "ODS_TIMETABLE".ods_timetable_act insert into (group_text,time_interval_act,time_interval_text) values ({group_text[i],time_interval_text[i],event_text[i]} '''))
+            for h in range(length(event_text)):
+                final_massive[h][0] = event_text[h]
+                final_massive[h][1] = time_interval_text[h]
+                final_massive[h][2] = group_text[h] 
+            return final_massive
+
+@task(task_id='download_pages_to_db', intlets=Dataset("STG_RASPHYSMSU.raw_html"), outlets =Dataset("ODS_TIMETABLE.ods_timetable_act"))
+def get_from_database_data():
+    data = []
+    DB_URI = Connection.get_connection_from_secrets('postgres_dwh').get_uri().replace("postgres://", "postgresql://")
+    sql_engine = sa.create_engine(DB_URI)
+    with sql_engine.connect() as conn:
+        data = conn.execute(sa.text(f'''SELECT * FROM "STG_RASPHYSMSU".raw_html''')).fetchall()
+        logging.info("starting parsing")
+        data = pd.DataFrame(parse_data(data))
+        data.to_sql(
+        "ods_timetable_act",
+        con=sql_engine,
+        schema="ODS_TIMETABLE",
+        if_exists="replace",
+        index=False,
+    )
+    return Dataset("ODS_TIMETABLE.ods_timetable_act")
 @dag(
     schedule='0 */1 * * *',
     start_date=datetime(2024, 1, 1, 2, 0, 0),
