7c3abc747c05f04217504d56cc472f03ede8ebe5
Stanislav Roslavtsev <jalchinn@yandex.ru>
2024-11-07 22:38:34 +0300

    Initial

---
+++ timetable/lectures.py
@@ -0,0 +1,55 @@
+import logging
+from datetime import datetime, timedelta
+
+from sqlalchemy import create_engine
+import requests as r
+import sqlalchemy as sa
+from airflow import DAG
+from airflow.datasets import Dataset
+from airflow.decorators import task
+from airflow.models import Connection, Variable
+import requests
+
+API_LINK = Variable.get("API_LINK")
+DWH_DB_DSN = (
+    Connection.get_connection_from_secrets("postgres_dwh")
+    .get_uri()
+    .replace("postgres://", "postgresql://")
+    .replace("?__extra__=%7B%7D", "")
+)
+TOKEN_ROBOT_TIMETABLE = Variable.get("TOKEN_ROBOT_TIMETABLE")
+
+
+@task(task_id="send_lecturers", retries=3)
+def send_lecturers():
+    dwh_sql_engine = create_engine(DWH_DB_DSN)
+    with dwh_sql_engine.connect() as dwh_conn:
+        lecturers = dwh_conn.execute(
+            sa.text(
+                """SELECT first_name, middle_name, last_name, description FROM "DM_TIMETABLE".dim_lecturer_act"""
+            )
+        ).fetchall()
+
+    for lectuter in lecturers:
+        body = {
+            "first_name": lectuter[0],
+            "middle_name": lectuter[1],
+            "last_name": lectuter[2],
+            "description": lectuter[3],
+        }
+        requests.post(
+            f"{API_LINK}/timetable/lecturer/",
+            headers={"Authorization": TOKEN_ROBOT_TIMETABLE},
+            body=body,
+        )
+
+
+with DAG(
+    dag_id="send_lecturers_timetable",
+    start_date=datetime(2022, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["dwh", "timetable"],
+    default_args={"owner": "roslavtsevsv"},
+) as dag:
+    send_lecturers()
