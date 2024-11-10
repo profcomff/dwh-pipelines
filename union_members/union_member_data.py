a8def528b1fec1ce6fbfd8fe075c772d7cb09f39
RedstoneEnjoyer <moremath10@gmail.com>
2024-11-10 14:03:02 +0300

    changed select * to select columns

--- union_members/union_member_data.py
+++ union_members/union_member_data.py
@@ -91,7 +91,8 @@ on conflict (user_id) do update set
 sql_merging_auth = """
 insert into "ODS_USER".info 
 (
-  user_id,
+  id,
+  auth_email,
   email,
   phone_number,
   vk_name,
@@ -115,10 +116,35 @@ insert into "ODS_USER".info
   sex,
   job,
   work_location,
-  is_deleted,
-  auth_email
+  is_deleted
 )
-select *
+select
+  user_id as id,
+  auth_email,
+  email,
+  phone_number,
+  vk_name,
+  city,
+  hometown,
+  location,
+  github_name,
+  telegram_name,
+  home_phone_number,
+  education_level,
+  university,
+  faculty,
+  "group",
+  position,
+  student_id_number,
+  department,
+  mode_of_study,
+  full_name,
+  birth_date,
+  photo,
+  sex,
+  job,
+  work_location,
+  is_deleted
 from "DWH_USER_INFO".info
 left join
 (
