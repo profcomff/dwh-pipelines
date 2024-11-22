ba77013b3b9f9c88586e86de4d50c0cab3df8b4e
RedstoneEnjoyer <moremath10@gmail.com>
2024-11-22 14:31:03 +0300

    added update set

--- union_members/union_member_data.py
+++ union_members/union_member_data.py
@@ -157,6 +157,32 @@ from "STG_AUTH".auth_method as au
 left join "STG_AUTH".user as u on u.id = au.user_id
 group by au.user_id
 ) using (user_id)
+on conflict (id) do update set
+	email = EXCLUDED.email,
+	phone_number = EXCLUDED.phone_number,
+	vk_name = EXCLUDED.vk_name,
+	city = EXCLUDED.city,
+	hometown = EXCLUDED.hometown,
+	location = EXCLUDED.location,
+	github_name = EXCLUDED.github_name,
+	telegram_name = EXCLUDED.telegram_name,
+	home_phone_number = EXCLUDED.home_phone_number,
+	education_level = EXCLUDED.education_level,
+	university = EXCLUDED.university,
+	faculty = EXCLUDED.faculty,
+	"group" = EXCLUDED."group",
+	position = EXCLUDED.position,
+	student_id_number = EXCLUDED.student_id_number,
+	department = EXCLUDED.department,
+	mode_of_study = EXCLUDED.mode_of_study,
+	full_name = EXCLUDED.full_name,
+	birth_date = EXCLUDED.birth_date,
+	photo = EXCLUDED.photo,
+	sex = EXCLUDED.sex,
+	job = EXCLUDED.job,
+	work_location = EXCLUDED.work_location,
+	student_id = EXCLUDED.student_id;
+	is_deleted = EXCLUDED.is_deleted
 """
 
 with DAG(
