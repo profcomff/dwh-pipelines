ffadd574d4a52bd56a63fcf02ca068aef88c8135
Парфенов Михаил Александрович <parfenovma@it.profcomff.com>
2024-11-30 12:15:16 +0000

    refactoring

---
+++ pipelines/dm/infra_logs/container_log_cube/__init__.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from textwrap import dedent
+
+from airflow import DAG, Dataset
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+
+with DAG(
+    dag_id="logs_cube",
+    start_date=datetime(2024, 1, 1),
+    schedule=[Dataset("ODS_INFRA_LOGS.container_log")],
+    catchup=False,
+    tags=["dwh", "dm", "infra", "logs"],
+    default_args={"owner": "dyakovri"},
+):
+    PostgresOperator(
+        postgres_conn_id="postgres_dwh",
+        sql=dedent(
+            r"""
+            with new_log as (
+                select
+                    coalesce (container_name, 'all') as container_name,
+                    coalesce(to_char(date_trunc('day', create_ts), 'YYYY-MM-DD'), 'all') as create_date,
+                    coalesce(sum(case when upper(coalesce(record #>> '{level_name}', record #>> '{loglevel}'))='DEBUG' then 1 else 0 end), 0) as debug_cnt,
+                    coalesce(sum(case when upper(coalesce(record #>> '{level_name}', record #>> '{loglevel}'))='WARNING' then 1 else 0 end), 0) as warn_cnt,
+                    coalesce(sum(case when upper(coalesce(record #>> '{level_name}', record #>> '{loglevel}'))='INFO' then 1 else 0 end), 0) as info_cnt,
+                    coalesce(sum(case when upper(coalesce(record #>> '{level_name}', record #>> '{loglevel}'))='ERROR' then 1 else 0 end), 0) as error_cnt,
+                    coalesce(sum(case when upper(coalesce(record #>> '{level_name}', record #>> '{loglevel}'))='CRITICAL' then 1 else 0 end), 0) as critical_cnt,
+                    coalesce(sum(case when upper(coalesce(record #>> '{level_name}', record #>> '{loglevel}')) <> ALL ('{"DEBUG", "WARNING", "INFO", "ERROR", "CRITICAL"}') then 1 else 0 end), 0) as other_cnt
+                from "ODS_INFRA_LOGS".container_log
+                where
+                    container_name like 'com_profcomff_api_%'
+                    and upper(coalesce(record #>> '{level_name}', record #>> '{loglevel}')) is not null
+                group by cube(container_name, date_trunc('day', create_ts))
+            )
+            merge into "DM_INFRA_LOGS".container_log_cube as clc
+            using new_log as nl
+                on nl.container_name = clc.container_name and nl.create_date = clc.create_date
+            when matched and not (nl.container_name = 'all' and nl.create_date = 'all') then
+                update set
+                    debug_cnt = greatest(clc.debug_cnt, nl.debug_cnt),
+                    warn_cnt = greatest(clc.warn_cnt, nl.warn_cnt),
+                    info_cnt = greatest(clc.info_cnt, nl.info_cnt),
+                    error_cnt = greatest(clc.error_cnt, nl.error_cnt),
+                    critical_cnt = greatest(clc.critical_cnt, nl.critical_cnt),
+                    other_cnt = greatest(clc.other_cnt, nl.other_cnt)
+            when not matched and not (nl.container_name = 'all' and nl.create_date = 'all') then
+                insert (container_name, create_date, debug_cnt, warn_cnt, info_cnt, error_cnt, critical_cnt, other_cnt)
+                values (nl.container_name, nl.create_date, nl.debug_cnt, nl.warn_cnt, nl.info_cnt, nl.error_cnt, nl.critical_cnt, nl.other_cnt);
+        """
+        ),
+        task_id="execute_merge_statement",
+        inlets=[Dataset("ODS_INFRA_LOGS.container_log")],
+        outlets=[Dataset("DM_INFRA_LOGS.container_log_cube")],
+    )
--- pipelines/ods/auth/auth_method/__init__.py
+++ pipelines/ods/auth/auth_method/__init__.py
@@ -8,61 +8,6 @@ from datetime import datetime
 from airflow import DAG
 
 
-
-with DAG(
-    dag_id = 'ODS_AUTH.user',
-    start_date = datetime(2024, 11, 3),
-    schedule=[Dataset("STG_AUTH.user")],
-    catchup=False,
-    tags=["ods", "src", "auth"],
-    description='scd2_user_hist',
-    default_args = {
-        'retries': 1,
-        'owner':'mixx3',
-    },
-):
-    PostgresOperator(
-        task_id='user_hist',
-        postgres_conn_id="postgres_dwh",
-        sql=dedent("""
-        -- close records
-        update "ODS_AUTH".user as am
-        set valid_to_dt = '{{ ds }}'::Date
-        where am.id NOT IN(
-            select ods.id from
-                (select 
-                    id,
-                    create_ts,
-                    update_ts,
-                    is_deleted
-                from "ODS_AUTH".user
-                ) as ods
-            join "STG_AUTH".user as stg
-            on ods.id = stg.id
-            and ods.create_ts = stg.create_ts
-            and ods.update_ts = stg.update_ts
-            and ods.is_deleted = stg.is_deleted
-        );
-
-        --evaluate increment
-        insert into "ODS_AUTH".user
-        select 
-            stg.*,
-            '{{ ds }}'::Date,
-            null
-            from "STG_AUTH".user as stg
-            full outer join "ODS_AUTH".user as ods
-              on stg.id = ods.id
-            where 
-              ods.id is NULL
-              or ods.valid_to_dt='{{ ds }}'::Date
-        LIMIT 100000; -- чтобы не раздуло
-        """),
-        inlets = [Dataset("STG_AUTH.user")],
-        outlets = [Dataset("ODS_AUTH.user")],
-    )
-
-
 with DAG(
     dag_id = 'ODS_AUTH.auth_method',
     start_date = datetime(2024, 11, 3),
