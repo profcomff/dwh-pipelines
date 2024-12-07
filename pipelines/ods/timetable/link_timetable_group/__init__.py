e59edd5265b612bdaaa90170e92e77af6ad34c42
parfenovma <mmikee00800@gmail.com>
2024-12-07 22:38:21 +0300

    init

---
+++ pipelines/ods/timetable/link_timetable_group/__init__.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from textwrap import dedent
+
+from airflow import DAG, Dataset
+from airflow.decorators import task
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+
+with DAG(
+    dag_id="ODS_TIMETABLE.ods_link_timetable_group",
+    start_date=datetime(2024, 12, 1),
+    schedule=[Dataset("ODS_TIMETABLE.ods_timetable_act")],
+    catchup=False,
+    tags=["ods", "core", "timetable", "link_timetable_group"],
+    default_args={"owner": "mixx3"},
+):
+    PostgresOperator(
+        postgres_conn_id="postgres_dwh",
+        sql=dedent(r"""
+            -- truncate old state
+            delete from "ODS_TIMETABLE".ods_link_timetable_group;
+
+            insert into "ODS_TIMETABLE".ods_link_timetable_group (
+                "group",
+                event_tr,
+                group_id,
+            )
+            select 
+            *
+            from(
+            select 
+                event."group" as "group",
+                event.id as event_tr,
+                "group".id as group_id,
+            -- оконка чтобы отобрать самое лучшее совпадение по триграмме
+            row_number() 
+            over (
+                partition by event.id -- чтобы потом дедублицировать
+                -- подробнее про метод: https://habr.com/ru/companies/otus/articles/770674/
+                order by to_tsvector('russian', event.name) @@ plainto_tsquery('russian', "group".group_number) desc
+            ) as rn
+            from "ODS_TIMETABLE".ods_timetable_act as event
+            cross join "DM_TIMETABLE".dim_group_act as "group"
+            )
+            where rn = 1
+        """),
+        task_id="execute_query",
+        inlets=[Dataset("ODS_TIMETABLE.ods_timetable_act"), Dataset("DM_TIMETABLE.dim_group_act")],
+        outlets=[Dataset("ODS_TIMETABLE.ods_link_timetable_group")],
+    )
---
+++ pipelines/ods/timetable/link_timetable_lesson/__init__.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from textwrap import dedent
+
+from airflow import DAG, Dataset
+from airflow.decorators import task
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+
+with DAG(
+    dag_id="ODS_TIMETABLE.ods_link_timetable_dim_event",
+    start_date=datetime(2024, 12, 1),
+    schedule=[Dataset("ODS_TIMETABLE.ods_timetable_act")],
+    catchup=False,
+    tags=["ods", "core", "timetable", "link_timetable_dim_event"],
+    default_args={"owner": "mixx3"},
+):
+    PostgresOperator(
+        postgres_conn_id="postgres_dwh",
+        sql=dedent(r"""
+            -- truncate old state
+            delete from "ODS_TIMETABLE".ods_link_timetable_dim_event;
+
+            select 
+                *
+                from(
+                    select 
+                        event."group" as "group",
+                        event.id as event_tr,
+                        dim_event.id as group_id,
+                    -- оконка чтобы отобрать самое лучшее совпадение по триграмме
+                    row_number() 
+                    over (
+                        partition by event.id -- чтобы потом дедублицировать
+                        -- подробнее про метод: https://habr.com/ru/companies/otus/articles/770674/
+                        order by to_tsvector('russian', event.name) @@ plainto_tsquery('russian', dim_event.event_name_text) desc
+                    ) as rn
+                    from "ODS_TIMETABLE".ods_timetable_act as event
+                    cross join (
+                        select 
+                            max(id) as id,  
+                            event_name_text
+                        from "DM_TIMETABLE".dim_event_act
+                        where source_name = 'profcomff_timetable_api'
+                        group by 
+                            event_name_text
+                    ) as dim_event
+                )
+            where rn = 1
+        """),
+        task_id="execute_query",
+        inlets=[Dataset("ODS_TIMETABLE.ods_timetable_act"), Dataset("DM_TIMETABLE.dim_event_act")],
+        outlets=[Dataset("ODS_TIMETABLE.ods_link_timetable_lesson")],
+    )
