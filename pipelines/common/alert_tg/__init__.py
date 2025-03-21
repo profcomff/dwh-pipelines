8758722d4a12bdcc7f35eef5f97c5611dc105064
DROPDATABASE <you@example.com>
2025-03-21 20:50:31 +0300

    From rating api instead sql

--- pipelines/common/alert_tg/__init__.py
+++ pipelines/common/alert_tg/__init__.py
@@ -1,72 +1,68 @@
 import json
 import logging
 from datetime import datetime
-from textwrap import dedent
 
 import requests
-import sqlalchemy as sa
 from airflow import DAG
-from airflow.datasets import Dataset
 from airflow.decorators import task
-from airflow.models import Connection, Variable
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from sqlalchemy import create_engine
-
-DWH_DB_DSN = (
-    Connection.get_connection_from_secrets("postgres_dwh")
-    .get_uri()
-    .replace("postgres://", "postgresql://")
-    .replace("?__extra__=%7B%7D", "")
-)
+from airflow.models import Variable
 
 
 @task(task_id="send_alert_pending_comments", retries=3)
 def send_alert_pending_comments():
-    dwh_sql_engine = create_engine(DWH_DB_DSN)
-    batch_size = 10  # Количество строк в одном батче
+    token_bot = str(Variable.get("TGBOT_TOKEN"))
+    token_auth = str(Variable.get(""))
 
-    token = str(Variable.get("TGBOT_TOKEN"))
+    API_URLS = {  # Список Адресов
+        "development": "http://localhost:8000/comment",
+        "testing": "...",
+        "production": "...",
+    }
+    API_URL: str = API_URLS.get(
+        str(Variable.get("_ENVIRONMENT")),
+        API_URLS["development"],  # Получаем url через переменную в окружении
+    )
 
-    with dwh_sql_engine.connect() as dwh_conn:
-        result = dwh_conn.execution_options(stream_results=True).execute(
-            sa.text(
-                """
-                SELECT uuid, user_id, subject
-                FROM "STG_TIMETABLE".comment
-                WHERE review_status = 'PENDING';
-            """
-            )
-        )
+    batch_size = 3  # Количество строк в одном батче
+    payload = {"limit": batch_size, "offset": 0, "unreviewed": True}
+    headers = {"Authorization": token_auth, "accept": "application/json"}
 
-        while True:
-            body = {
-                "comments": [],
+    def fetch_comments():
+        """Запрашивает комментарии и возвращает их в виде списка кортежей (uuid, user_id, subject)."""
+        response = requests.get(API_URL, params=payload, headers=headers)
+        if response.status_code != 200:
+            logging.error("Ошибка запроса: %s", response.text)
+            return []
+
+        return response.json().get("comments", [])
+
+    while True:
+        comments = fetch_comments()
+        if not comments:
+            logging.info("No pending comments")
+            break
+
+        comments_ans = [
+            {
+                "comment_uuid": comment["uuid"],
+                "user_id": f"👤 Автор_id: {comment["user_id"]}",
+                "subject": f'💬 Текст: "{comment["subject"]}"',
+                "url": f"🔗 {API_URL}/{comment["uuid"]}",
             }
+            for comment in comments
+        ]
 
-            comments = result.fetchmany(
-                batch_size
-            )  # Получаем `batch_size` строк за раз
-            if not comments:
-                logging.info("No pending comments")
-                break
+        message_json = json.dumps(comments_ans)
+        logging.info(message_json)
 
-            for comment in comments:  # [(uuid, user_id, subject), ]
-                comment_dct = {
-                    "comment_uuid": str(comment[0]),
-                    "user_id": f"👤 Автор_id: {comment[1]}",
-                    "subject": f'💬 Текст: "{comment[2]}"',
-                    "url": f"🔗 {None}",
-                }
-                body["comments"] += [comment_dct]
+        req = requests.post(
+            f"https://api.telegram.org/bot{token_bot}/sendMessage",
+            json=message_json,
+        )
+        logging.info("Bot send message status %d (%s)", req.status_code, req.text)
+        req.raise_for_status()
 
-            message_json = json.dumps(body)
-            logging.info(message_json)
-            req = requests.post(
-                f"https://api.telegram.org/bot{token}/sendMessage",
-                json=message_json,
-            )
-            logging.info("Bot send message status %d (%s)", req.status_code, req.text)
-            req.raise_for_status()
+        payload["offset"] += batch_size
 
 
 with DAG(
