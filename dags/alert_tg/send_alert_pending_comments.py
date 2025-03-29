ed1c9d49e57c4abb02774983d9d38d4060c5438c
redstoneenjoyer <moremath10@gmail.com>
2025-03-29 10:35:38 +0300

    reworking stucture

---
+++ dags/alert_tg/send_alert_pending_comments.py
@@ -0,0 +1,59 @@
+import logging
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+
+from pipelines.common.alert_tg.config import batch_size, get_api_url, get_env_variable
+from pipelines.common.alert_tg.utils.fetch_comments import fetch_comments
+from pipelines.common.alert_tg.utils.send_telegram import send_comments
+
+
+@task(task_id="send_alert_pending_comments", retries=3)
+def send_alert_pending_comments():
+    payload = {"limit": batch_size, "offset": 0, "unreviewed": True}
+
+    if str(get_env_variable("_ENVIRONMENT")) == "prod":
+        while True:
+            comments = fetch_comments(payload)
+            if not comments:
+                logging.info("No pending comments")
+                break
+
+            comments_ans = "\n\n".join(
+                f"UUID: {comment['uuid']} \n 👤 Автор_id: {comment['user_id']} \n 💬 Текст: \"{comment['subject']}\" \n 🔗 {get_api_url()}/{comment['uuid']}"
+                for comment in comments
+            )
+
+            send_comments(comments_ans)  # Отправка в бота
+            payload["offset"] += batch_size
+
+    else:
+        count_comments = 0
+        while True:
+            comments = fetch_comments(payload)
+            if not comments:
+                break
+
+            count_comments += len(comments)
+            payload["offset"] += batch_size
+
+        if str(get_env_variable("_ENVIRONMENT")) == "test":
+            send_comments(
+                f"TEST: {count_comments} новых комметариев"
+            )  # Отправка в бота
+        else:  # Логика для локального запуска или разработки
+            logging.info("Running in local environment")
+            print(f"INFO: {count_comments} новых комметариев")
+            logging.info(f"INFO: {count_comments} новых комметариев")
+
+
+with DAG(
+    dag_id="send_alert_pending_comments",
+    start_date=datetime(2022, 1, 1),
+    schedule_interval="*/10 * * * *",  # Каждые 10 минут
+    catchup=False,
+    tags=["dwh", "comments"],
+    default_args={"owner": "DROPDATABASE"},
+) as dag:
+    send_alert_pending_comments()
---
+++ dags/common/get_vds_balance.py
@@ -0,0 +1,94 @@
+import logging
+from datetime import datetime, timedelta
+
+import requests as r
+from airflow import DAG
+from airflow.decorators import task
+from airflow.models import Variable
+
+ENVIRONMENT = Variable.get("_ENVIRONMENT")
+
+def send_message_on_failure(context):
+    token = str(Variable.get("TGBOT_TOKEN"))
+    chat_id = int(Variable.get("TG_CHAT_DWH"))
+    # Параметры сообщения
+    dag_id = context['dag'].dag_id
+    owner = context['dag'].owner
+    
+    message = f"🚨 DAG Failed 🚨\n\nDAG ID {dag_url}: {dag_id}\nOwner: {owner}"
+    tg_api_url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+    if ENVIRONMENT == "prod":
+        dag_url = f"https://airflow.profcomff.com/dags/{dag_id}/grid"
+    else:
+        dag_url = f"https://airflow.test.profcomff.com/dags/{dag_id}/grid" 
+
+
+    msg = {
+        "chat_id": chat_id,
+        "text": message,
+        "parse_mode": "MarkdownV2",
+    }
+    logging.info(msg)
+    # Отправлка сообщения через api телеграма
+    try:
+        req = r.post(tg_api_url, json=msg, timeout=10)
+        req.raise_for_status()
+    except Exception as e:
+        logging.error(f"Telegram API error: {str(e)}")
+
+    logging.info("Bot send message status %d (%s)", req.status_code, req.text)
+
+@task(task_id="send_telegram_message", retries=3)
+def send_telegram_message_or_print(chat_id, balance):
+    """Скачать данные из ЛК ОПК"""
+
+    if balance > 700:
+        logging.info(f"Баланс {balance} рублей")
+    else:
+        token = str(Variable.get("TGBOT_TOKEN"))
+        r.post(
+            f"https://api.telegram.org/bot{token}/sendMessage",
+            json={
+                "chat_id": chat_id,
+                "text": f"Баланс vds.sh составляет {balance} рублей. Время пополнить счет, @MArzangulyan!",
+            },
+        )
+
+
+@task(task_id="fetch_users", retries=3)
+def get_balance():
+    """Скачать данные из ЛК ОПК"""
+
+    with r.Session() as s:
+        username = str(Variable.get("LK_VDSSH_ADMIN_USERNAME"))
+        password = str(Variable.get("LK_VDSSH_ADMIN_PASSWORD"))
+        resp = s.get(
+            f"https://my.vds.sh/manager?out=sjson&func=auth&username={username}&password={password}"
+        )
+        auth_id = resp.json()["doc"]["auth"]["$id"]
+
+        resp = s.get(
+            f"https://my.vds.sh/manager?out=sjson&func=dashboard.info&auth={auth_id}"
+        )
+        balance = resp.json()["doc"]["elem"][0]["balance"][0]["$"]
+        balance = float(balance.split()[0])
+
+    return balance
+
+
+with DAG(
+    dag_id="check_vds_balance",
+    schedule="0 */12 * * *",
+    start_date=datetime(2023, 1, 1, 2, 0, 0),
+    catchup=False,
+    tags=["infra"],
+    default_args={
+        "owner": "dyakovri",
+        "retries": 3,
+        "retry_delay": timedelta(minutes=5),
+        "on_failure_callback": send_message_on_failure,
+    },
+) as dag:
+    balance = get_balance()
+    send_telegram_message_or_print(int(Variable.get("TG_CHAT_MANAGERS")), balance)
