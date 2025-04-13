598e44f3db27e76e93f6bbb782fa2923adfe197d
redstoneenjoyer <moremath10@gmail.com>
2025-04-13 15:52:40 +0300

    reworked dwh user info dag

---
+++ dags/dwh/user/info.py
@@ -0,0 +1,28 @@
+import logging
+from datetime import datetime
+from textwrap import dedent
+
+from airflow import DAG
+from airflow.datasets import Dataset
+from airflow.decorators import dag, task
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+with DAG(
+    dag_id="DWH_USER_INFO.info",
+    start_date=datetime(2024, 10, 1),
+    schedule=[Dataset("STG_USERDATA.info"), Dataset("STG_USERDATA.param")],
+    catchup=False,
+    tags=["dwh", "core", "user_info"],
+    description="union_members_data_format_correction",
+    default_args={
+        "retries": 1,
+        "owner": "redstoneenjoyer",
+    },
+) as dag:
+    run_sql = PostgresOperator(
+        task_id="execute_sql",
+        postgres_conn_id="postgres_dwh",
+        sql='upsert.sql',
+        inlets=[Dataset("STG_USERDATA.info"), Dataset("STG_USERDATA.param")],
+        outlets=[Dataset("DWH_USER_INFO.info")],
+    )
