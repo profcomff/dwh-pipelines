75ba3a967de8d32d3c34aca6dac77faad42f98c0
redstoneenjoyer <moremath10@gmail.com>
2025-04-16 18:58:32 +0300

    decorated import functions

--- dags/stg/copy_api_tables/upload_api_achievement.py
+++ dags/stg/copy_api_tables/upload_api_achievement.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.datasets import Dataset
 from airflow.models import Variable
+
 from plugins.api_utils import send_telegram_message as send_msg, copy_table_to_dwh as copy_tbl
 
 @task(task_id="send_telegram_message", trigger_rule="one_failed")
