4a700377f4a96b0c9506aad386023790c9ae2bd1
redstoneenjoyer <moremath10@gmail.com>
2025-04-16 16:53:42 +0300

    trying to rework stg

---
+++ dags/stg/copy_api_tables/upload_api_achievement.py
@@ -0,0 +1,33 @@
+from copy_api_tables import *
+
+with DAG(
+    dag_id="upload_api_achievement",
+    start_date=datetime(2024, 1, 1),
+    schedule="@daily",
+    catchup=False,
+    tags=["dwh", "stg", "achievement"],
+    default_args={
+        "owner": "dyakovri",
+        "retries": 5,
+        "retry_delay": timedelta(minutes=3),
+        "retry_exponential_backoff": True,
+    },
+):
+    tables = "achievement", "achievement_reciever"
+    tg_task = send_telegram_message(int(Variable.get("TG_CHAT_DWH")))
+    prev = None
+    for table in tables:
+        curr = copy_table_to_dwh.override(
+            task_id=f"copy-{table}",
+            outlets=[Dataset(f"STG_ACHIEVEMENT.{table}")],
+        )(
+            "api_achievement",
+            table,
+            "STG_ACHIEVEMENT",
+            table,
+        )
+        # Выставляем копирование по порядку
+        if prev:
+            prev >> curr
+        prev = curr
+        prev >> tg_task
