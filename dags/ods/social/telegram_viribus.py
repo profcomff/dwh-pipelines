9dca8e9f01430002d78c093719320dfc671570cc
parfenovma <parfenov.ma21@physics.msu.ru>
2025-04-16 07:13:03 +0000

    stg

--- dags/ods/social/telegram_viribus.py
+++ dags/ods/social/telegram_viribus.py
@@ -7,7 +7,7 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 with DAG(
     dag_id="ODS_SOCIAL.viribus_chat",
-    schedule=[Dataset("TG_SOCIAL.webhook_storage")],
+    schedule=[Dataset("STG_SOCIAL.webhook_storage")],
     start_date=datetime(2024, 11, 3),
     catchup=False,
     tags=["ods", "core", "social", "telegram"],
