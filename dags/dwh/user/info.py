36e0b8b5c53a388f8d93c3fdf1cb14574d2703b6
redstoneenjoyer <moremath10@gmail.com>
2025-04-16 15:31:55 +0300

    task docs to dwh_user_info.info

--- dags/dwh/user/info.py
+++ dags/dwh/user/info.py
@@ -7,6 +7,13 @@ from airflow.datasets import Dataset
 from airflow.decorators import dag, task
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
+def get_sql_code(filename):
+    directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(directory, 'info.sql')
+    with open(file_path, 'r') as f:
+        sql_code = f.read()
+    return f"```SQL\n{sql_code}\n```"
+
 with DAG(
     dag_id="DWH_USER_INFO.info",
     start_date=datetime(2024, 10, 1),
@@ -23,6 +30,7 @@ with DAG(
         task_id="execute_sql",
         postgres_conn_id="postgres_dwh",
         sql="info.sql",
+        doc_md=get_sql_code('info.sql'),
         inlets=[Dataset("STG_USERDATA.info"), Dataset("STG_USERDATA.param")],
         outlets=[Dataset("DWH_USER_INFO.info")],
     )
