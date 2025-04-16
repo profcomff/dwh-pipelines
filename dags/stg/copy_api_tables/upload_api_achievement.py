d78180659c1fadb4ecbe6668b61fedd4cc3cbfed
redstoneenjoyer <moremath10@gmail.com>
2025-04-16 18:58:32 +0300

    decorated import functions

--- dags/stg/copy_api_tables/upload_api_achievement.py
+++ dags/stg/copy_api_tables/upload_api_achievement.py
@@ -3,7 +3,15 @@ from airflow import DAG
 from airflow.datasets import Dataset
 from airflow.models import Variable
 
-from plugins.api_utils import send_telegram_message, copy_table_to_dwh
+from plugins.api_utils import send_telegram_message as send_msg, copy_table_to_dwh as copy_tbl
+
+@task(task_id="send_telegram_message", trigger_rule="one_failed")
+def send_telegram_message(chat_id, **context):
+    return send_msg(chat_id, **context)
+
+@task(task_id="copy_table_to_dwh", trigger_rule="one_done", retries=0)
+def copy_table_to_dwh(from_schema, from_table, to_schema, to_table):
+    return copy_tbl(from_schema, from_table, to_schema, to_table)
 
 with DAG(
     dag_id="upload_api_achievement",
