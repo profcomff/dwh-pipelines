927b66936e4e04245eee2708193f63ca9ade584f
parfenovma <parfenov.ma21@physics.msu.ru>
2025-04-17 19:53:09 +0000

    add cols in markeintg

--- pipelines/dm/marketing/__init__.py
+++ pipelines/dm/marketing/__init__.py
@@ -23,7 +23,7 @@ with DAG(
             r"""
             DELETE FROM "DM_MARKETING".frontend_actions_services;
             INSERT INTO "DM_MARKETING".frontend_actions_services
-            (uuid, user_id, action, path_from, path_to, user_agent, is_bot, create_ts, service_name)
+            (uuid, user_id, action, path_from, path_to, user_agent, is_bot, create_ts, service_from_name, service_to_name)
             SELECT
                 fa.uuid,
                 fa.user_id,
@@ -33,14 +33,18 @@ with DAG(
                 fa.user_agent,
                 fa.is_bot,
                 fa.create_ts,
-                fa.name
+                fa.name_from,
+				fa.name_to
             FROM (
                 SELECT 
                     fa.*,
-                    b.name 
+                    b_to.name as name_to,
+					b_from.name as name_from
                 FROM "ODS_MARKETING".frontend_actions AS fa
-                JOIN "STG_SERVICES".button AS b
-                    ON SPLIT_PART(fa.path_to::text, '/', -1) = b.id::text
+                LEFT JOIN "STG_SERVICES".button AS b_to
+                    ON SPLIT_PART(fa.path_to::text, '/', -1) = b_to.id::text
+				LEFT JOIN "STG_SERVICES".button AS b_from
+					ON SPLIT_PART(fa.path_from::text, '/', -1) = b_from.id::text
                 WHERE fa.path_to ILIKE '%apps%'
             ) AS fa;
         """
