20bd6c4ec37c00d53d8fb80a0673416b528f1815
redstoneenjoyer <moremath10@gmail.com>
2025-04-17 13:37:49 +0300

    black

--- dags/stg/copy_api_tables/upload_api_social.py
+++ dags/stg/copy_api_tables/upload_api_social.py
@@ -4,15 +4,21 @@ from airflow.datasets import Dataset
 from airflow.decorators import task
 from airflow.models import Variable
 
-from plugins.api_utils import send_telegram_message as send_msg, copy_table_to_dwh as copy_tbl
+from plugins.api_utils import (
+    send_telegram_message as send_msg,
+    copy_table_to_dwh as copy_tbl,
+)
+
 
 @task(task_id="send_telegram_message", trigger_rule="one_failed")
 def send_telegram_message(chat_id, **context):
     return send_msg(chat_id, **context)
 
+
 @task(task_id="copy_table_to_dwh", trigger_rule="one_done", retries=0)
 def copy_table_to_dwh(from_schema, from_table, to_schema, to_table):
     return copy_tbl(from_schema, from_table, to_schema, to_table)
+
 
 with DAG(
     dag_id="upload_api_social",
@@ -53,4 +59,3 @@ with DAG(
             prev >> curr
         prev = curr
         prev >> tg_task
-    
