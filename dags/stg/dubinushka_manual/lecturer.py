541f29d994e3bccf9b235fd56e8d5c1d0bf6bfbb
redstoneenjoyer <moremath10@gmail.com>
2025-04-18 22:25:22 +0300

    dubinushka manual

--- dags/stg/dubinushka_manual/lecturer.py
+++ dags/stg/dubinushka_manual/lecturer.py
@@ -22,19 +22,3 @@ with DAG(
             task_id="upload_lecturers_from_backup",
             outlets=[Dataset("STG_DUBINUSHKA_MANUAL.lecturer")],
         )
-
-with DAG(
-    dag_id="STG_DUBINUSHKA_MANUAL.comment",
-    start_date=datetime(2024, 1, 1),
-    schedule="@once",
-    catchup=False,
-    tags=["stg", "src", "dubinushka", "lecturer"],
-    default_args={"owner": "mixx3"},
-):
-    with open(f"{CUR_DIR}/comments.sql", "r") as sql_query_file:
-        PostgresOperator(
-            postgres_conn_id="postgres_dwh",
-            sql=sql_query_file.read(),
-            task_id="upload_comments_from_backup",
-            outlets=[Dataset("STG_DUBINUSHKA_MANUAL.comment")],
-        )
