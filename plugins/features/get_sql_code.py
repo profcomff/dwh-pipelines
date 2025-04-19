f0eaaa616ea6cf7e2ee38f8bfad42a0576647f7c
redstoneenjoyer <moremath10@gmail.com>
2025-04-19 13:35:29 +0300

    fuction argument fix

--- plugins/features/get_sql_code.py
+++ plugins/features/get_sql_code.py
@@ -2,7 +2,7 @@ import os
 
 def get_sql_code(filename):
     directory = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(directory, 'info.sql')
+    file_path = os.path.join(directory, filename)
     with open(file_path, 'r') as f:
         sql_code = f.read()
     return f"```SQL\n{sql_code}\n```"
