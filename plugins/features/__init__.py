2706a0e0eea8c7f098852dcf0f948ff83578fcd3
RedstoneEnjoyer <151675542+RedstoneEnjoyer@users.noreply.github.com>
2025-04-24 18:16:55 +0300

    Common + test changed (#221)
    **Изменения в тестах:**
    убрал проверку на количество дагов (раньше тест фейлился если дагов нет)
    теперь они проверяют название последнего коммита на наличие skip-ci  
    
    **Добавил алерты**
    Есть нербочий даг get_vds_balance на который я прикрутил алерты в
    телеграм чтобы протестить их
    
    **Рефакториг**
    Перенес половину слоя common

--- plugins/features/__init__.py
+++ plugins/features/__init__.py
@@ -1,1 +1,2 @@
 from .get_sql_code import get_sql_code
+from .alert_message import alert_message
---
+++ plugins/features/alert_message.py
@@ -0,0 +1,39 @@
+import logging
+from airflow.decorators import dag, task
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.datasets import Dataset
+
+from airflow import DAG
+
+from airflow.models import Variable
+import requests as r
+
+ENVIRONMENT = Variable.get("_ENVIRONMENT")
+TOKEN = str(Variable.get("TGBOT_TOKEN"))
+
+
+def alert_message(context, chat_id: int | str):
+    # Параметры сообщения
+    dag_id = context['dag'].dag_id
+    owner = context['dag'].owner
+    dag_url = f"https://airflow.{'test' if not ENVIRONMENT == 'prod' else ''}.profcomff.com/dags/{dag_id}/grid"
+
+    # Дебильные эмодзи чтобы раздражать людей
+    message = f"🚨 *DAG Failed* 🚨 🗣🗣🗣\n\n*DAG ID*: {dag_id}\n*Owner*: {owner}\n*Dag URL*: {dag_url}" 
+    tg_api_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    msg = {
+        "chat_id": chat_id,
+        "text": message,
+        "parse_mode": "MarkdownV2",
+    }
+    logging.debug(msg)
+    # Отправлка сообщения через api телеграма
+    try:
+        req : requests.Response = r.post(url=tg_api_url, json=msg, timeout=10)
+        req.raise_for_status()
+    except Exception as e:
+        logging.error(f"Telegram API error: {str(e)}")
+
+    logging.info("Bot send message status %d (%s)", req.status_code, req.text)
+    req.raise_for_status()
