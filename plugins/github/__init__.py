52c554cb0e187c010f0ff0ff0a2b9b69d91a7798
redstoneenjoyer <moremath10@gmail.com>
2025-04-24 18:47:05 +0300

    шыщке фпфшт

--- plugins/github/__init__.py
+++ plugins/github/__init__.py
@@ -1,10 +1,3 @@
-from .fetch import (
-    fetch_gh_invations,
-    fetch_gh_members,
-    fetch_gh_org,
-    fetch_gh_repos,
-    fetch_gh_teams,
-    get_conn,
-    upload_df,
-)
+from .fetch import (fetch_gh_invations, fetch_gh_members, fetch_gh_org,
+                    fetch_gh_repos, fetch_gh_teams, get_conn, upload_df)
 from .get import flatten, get_all_gh_data, get_gh_data, get_organization
