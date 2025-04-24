f4e3838ce7c32c9586a129c17a4724472f279df0
redstoneenjoyer <moremath10@gmail.com>
2025-04-25 00:14:10 +0300

    экранирование

--- plugins/features/alert_message.py
+++ plugins/features/alert_message.py
@@ -15,14 +15,22 @@ TOKEN = str(Variable.get("TGBOT_TOKEN"))
 def alert_message(context, chat_id: int | str):
     # Параметры сообщения
     dag_id = context["dag"].dag_id
+    dag_id = (
+        dag_id.replace("=", "\\=").replace("-", "\\-").replace("+", "\\+").replace(".", "\\.").replace("_", "\\_")
+    )
+
     owner = context["dag"].owner
+    owner = (
+        owner.replace("=", "\\=").replace("-", "\\-").replace("+", "\\+").replace(".", "\\.").replace("_", "\\_")
+    )
+
     dag_url = f"https://airflow.{'test.' if not ENVIRONMENT == 'prod' else ''}profcomff.com/dags/{dag_id}/grid"
     dag_url = (
         dag_url.replace("=", "\\=").replace("-", "\\-").replace("+", "\\+").replace(".", "\\.").replace("_", "\\_")
     )
 
     # Дебильные эмодзи чтобы раздражать людей
-    message = f"*DAG Failed*\n\n*DAG ID*: {dag_id}\n*Owner*: {owner}\n[*Dag URL*]({dag_url})"
+    message = f"*DAG Failed*\n\n*DAG ID*: {dag_id}\n*Owner*: {owner}\n[Open in Airflow]({dag_url})"
     tg_api_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
     msg = {
