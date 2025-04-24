072efba3e0954fb15f1ed14dd3466255542a6064
DROPDATABASE <you@example.com>
2025-04-24 15:00:32 +0300

    Make format

--- pipelines/common/alert_tg/send_alert_pending_comments.py
+++ pipelines/common/alert_tg/send_alert_pending_comments.py
@@ -1,18 +1,24 @@
-import logging
 import datetime
+import logging
 
 from airflow import DAG
 from airflow.decorators import task
 
-from pipelines.common.alert_tg.config import batch_size, get_app_url, get_env_variable, set_env_variable
+from pipelines.common.alert_tg.config import (batch_size, get_app_url,
+                                              get_env_variable,
+                                              set_env_variable)
 from pipelines.common.alert_tg.utils.fetch_comments import fetch_comments
 from pipelines.common.alert_tg.utils.send_telegram import send_comments
 
 
 @task(task_id="send_alert_pending_comments", retries=3)
 def send_alert_pending_comments():
-    last_run_ts = datetime.datetime.fromisoformat(str(get_env_variable("last_run_ts_alert_tg")))  # Получаем время последнего запуска
-    set_env_variable("last_run_ts_alert_tg", str(datetime.datetime.today()))  # Устанавливаем время запуска последней проверки
+    last_run_ts = datetime.datetime.fromisoformat(
+        str(get_env_variable("last_run_ts_alert_tg"))
+    )  # Получаем время последнего запуска
+    set_env_variable(
+        "last_run_ts_alert_tg", str(datetime.datetime.today())
+    )  # Устанавливаем время запуска последней проверки
 
     payload = {"limit": batch_size, "offset": 0, "review_mode": "pending"}
     is_monday = datetime.datetime.today().weekday() == 0
@@ -28,25 +34,35 @@ def send_alert_pending_comments():
                 logging.info("No pending comments")
                 break
 
-            if is_monday == 0: break  # Выходим и возвращаем только кол-во
+            if is_monday == 0:
+                break  # Выходим и возвращаем только кол-во
 
             comments_ans = []
             for comment in comments:
-                if (not is_monday and datetime.datetime.fromisoformat(comment['update_ts']) >= last_run_ts):  # Смотрим новые комментраии(инкрементальная проверка)
-                # if (not is_monday and yesterday <= datetime.datetime.fromisoformat(comment['update_ts']) <= now):
-                    comments_ans += [f"UUID: {comment['uuid']} \n 👤 Автор_id: {comment['user_id']} \n 💬 Предмет: \"{comment['subject']}\""]
+                if (
+                    not is_monday
+                    and datetime.datetime.fromisoformat(comment["update_ts"])
+                    >= last_run_ts
+                ):  # Смотрим новые комментраии(инкрементальная проверка)
+                    # if (not is_monday and yesterday <= datetime.datetime.fromisoformat(comment['update_ts']) <= now):
+                    comments_ans += [
+                        f"UUID: {comment['uuid']} \n 👤 Автор_id: {comment['user_id']} \n 💬 Предмет: \"{comment['subject']}\""
+                    ]
                     total_today += 1
             if comments_ans:
                 send_comments("\n\n".join(comments_ans))  # Отправка в бота
             payload["offset"] += batch_size
-        
-        result_message = ''
+
+        result_message = ""
         if not is_monday and total_today:
-            result_message += f'Сегодня не проверено: {total_unreviewed} шт.'
+            result_message += f"Сегодня не проверено: {total_unreviewed} шт."
         if is_monday:
-            result_message += f'Всего непроверенных комментариев: {total_unreviewed} шт.'
-        result_message += f'\n🔗 {get_app_url()}'
+            result_message += (
+                f"Всего непроверенных комментариев: {total_unreviewed} шт."
+            )
+        result_message += f"\n🔗 {get_app_url()}"
         send_comments(result_message)
+
 
 with DAG(
     dag_id="send_alert_pending_comments",
