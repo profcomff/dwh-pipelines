5fb576dc54308d98876b669eba04ca3071b373f3
Vladislav <2007.5v.voskoboinik.vladislav@gmail.com>
2025-05-07 01:22:11 +0300

    rental-ods

---
+++ dags/ods/rental/event.sql
@@ -0,0 +1,24 @@
+-- truncate old state
+delete from "ODS_RENTAL".event;
+
+insert into "ODS_RENTAL".event(
+    uuid,
+    user_id,
+    admin_id,
+    session_id,
+    action_type,
+    details,
+    create_ts,
+    api_id
+)
+select
+    gen_random_uuid() as uuid,
+    user_id,
+    admin_id, 
+    session_id,
+    action_type,
+    details,
+    stg_event.create_ts at time zone 'utc' at time zone 'Europe/Moscow' as create_ts,
+    id
+from "STG_RENTAL".event as stg_event
+limit 1000001;
---
+++ dags/ods/rental/item.sql
@@ -0,0 +1,16 @@
+-- truncate old states
+delete from "ODS_RENTAL".item;
+
+insert into "ODS_RENTAL".item(
+    uuid,
+    api_id,
+    type_id,
+    is_available
+)
+select 
+    gen_random_uuid() as uuid,
+    id,
+    type_id,
+    is_available
+from "STG_RENTAL".item
+limit 1000001;
