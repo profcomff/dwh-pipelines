f81d0efdfbf85e32675415ade7bab8ea6f7f1094
Vladislav <86772772+VladislavVoskoboinik@users.noreply.github.com>
2025-05-07 20:00:46 +0300

    rental-ods (#249)
    ## Изменения
    <!-- Опишите здесь на языке, понятном каждому, изменения, сделанные в
    исходном коде по пунктам. -->
    Добавлены даги на ods уровень рентала
    ## Детали реализации
    <!-- Здесь можно описать технические детали по пунктам. -->
    
    ## Check-List
    <!-- После сохранения у следующих полей появятся галочки, которые нужно
    проставить мышкой -->
    - [ ] Вы проверили свой код перед отправкой запроса?
    - [ ] Вы написали тесты к реализованным функциям?
    - [ ] Вы не забыли применить форматирование `black` и `isort` для
    _Back-End_ или `Prettier` для _Front-End_?

---
+++ dags/ods/rental/rental_session.sql
@@ -0,0 +1,30 @@
+-- truncate old states
+delete from "ODS_RENTAL".rental_session;
+
+insert into "ODS_RENTAL".rental_session(
+    uuid,
+    api_id,
+    user_id,
+    item_id,
+    admin_open_id,
+    admin_close_id,
+    reservation_ts,
+    start_ts,
+    end_ts,
+    actual_return_ts,
+    status
+)
+select 
+    gen_random_uuid() as uuid,
+    id,
+    user_id,
+    item_id,
+    admin_open_id,
+    admin_close_id,
+    reservation_ts at time zone 'utc' at time zone 'Europe/Moscow' as reservation_ts,
+    start_ts at time zone 'utc' at time zone 'Europe/Moscow' as start_ts,
+    end_ts at time zone 'utc' at time zone 'Europe/Moscow' as end_ts,
+    actual_return_ts at time zone 'utc' at time zone 'Europe/Moscow' as actual_return_ts,
+    status
+from "STG_RENTAL".rental_session
+limit 100000;
