b6994ecae88233f212754bc4522555b00b0a38a4
Vladislav <86772772+VladislavVoskoboinik@users.noreply.github.com>
2025-05-20 12:06:51 +0300

    Ssl cert fix (#258)
    ## Изменения
    
    <!-- Опишите здесь на языке, понятном каждому, изменения, сделанные в
    исходном коде по пунктам. -->
    Фикс дага проверки баланса vds. Поменялся json и логика авторизации
    ## Детали реализации
    <!-- Здесь можно описать технические детали по пунктам. -->
    
    ## Check-List
    <!-- После сохранения у следующих полей появятся галочки, которые нужно
    проставить мышкой -->
    - [ ] Вы проверили свой код перед отправкой запроса?
    - [ ] Вы написали тесты к реализованным функциям?
    - [ ] Вы не забыли применить форматирование `black` и `isort` для
    _Back-End_ или `Prettier` для _Front-End_?

--- dags/common/get_vds_balance.py
+++ dags/common/get_vds_balance.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from functools import partial
 
 import requests as r
-import certifi
+import urllib3
 from airflow import DAG
 from airflow.decorators import task
 from airflow.models import Variable
@@ -12,6 +12,7 @@ from plugins.features import alert_message
 
 
 ENVIRONMENT = Variable.get("_ENVIRONMENT")
+url = "https://my.vds.sh/manager/billmgr"
 
 
 @task(task_id="send_telegram_message", retries=3)
@@ -33,22 +34,39 @@ def send_telegram_message_or_print(chat_id, balance):
 
 @task(task_id="fetch_users", retries=3)
 def get_balance():
-    """Скачать данные из ЛК ОПК"""
+    """Скачать данные из ЛК VDS.sh"""
+    urllib3.disable_warnings()
 
-    with r.Session() as s:
+    with r.Session() as session:
         username = str(Variable.get("LK_VDSSH_ADMIN_USERNAME"))
         password = str(Variable.get("LK_VDSSH_ADMIN_PASSWORD"))
-        resp = s.get(
-            f"https://my.vds.sh/manager?out=sjson&func=auth&username={username}&password={password}",
-            verify=certifi.where(),
-        )
-        response_cookies = resp.cookies
-        resp = s.get(f"https://my.vds.sh/manager?out=sjson", cookies=response_cookies, verify=certifi.where())
-        logging.info(resp.json())
-        balance = resp.json()["doc"]["user"]["$balance"]
-        balance = float(balance)
 
-    return balance
+        session.get(url, verify=False)
+
+        login_data = {'func': 'auth', 'username': username, 'password': password}
+
+        auth_response = session.post(url, data=login_data, verify=False)
+        logging.info(f"Auth response status: {auth_response.status_code}")
+
+        try:
+            auth_json = auth_response.json()
+            if auth_json['doc']['$func'] == 'logon':
+                logging.error("Авторизация не удалась, все еще на странице входа")
+                return None
+        except Exception as e:
+            logging.info(f"Не удалось распарсить JSON ответа авторизации: {e}")
+
+        balance_params = {'func': 'desktop', 'startform': 'vds', 'out': 'xjson'}
+
+        balance_response = session.get(url, params=balance_params, verify=False)
+        logging.info(f"Balance response status: {balance_response.status_code}")
+        balance_data = balance_response.json()
+        balance = balance_data.get('doc', {}).get('user', {}).get('$balance', str())
+        if balance is None:
+            logging.info("Баланс не был получен")
+            raise ValueError
+
+        return balance
 
 
 with DAG(
