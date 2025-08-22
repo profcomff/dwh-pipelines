589469d2d3e55d45e41c93999e341465faa1e2bf
Vladislav <2007.5v.voskoboinik.vladislav@gmail.com>
2025-08-23 02:39:40 +0300

    fiX

--- dags/dwh/rating/copy_to_rating_api.py
+++ dags/dwh/rating/copy_to_rating_api.py
@@ -9,36 +9,20 @@ from airflow.decorators import task
 from airflow.models import Connection, Variable
 from sqlalchemy import create_engine
 
-from plugins.api_utils import send_telegram_message
+from plugins.api_utils import copy_rating_to_rating_api, send_telegram_message
 from plugins.features import alert_message
 
 
-# декорированные функции
+copy_rating_to_rating_api = task(task_id="copy_rating_to_rating_api", trigger_rule="one_done", retries=0)(
+    copy_rating_to_rating_api
+)
 send_telegram_message = task(task_id="send_telegram_message", trigger_rule="one_failed")(send_telegram_message)
-
-
-API_DB_DSN = (
-    Connection.get_connection_from_secrets("postgres_api")
-    .get_uri()
-    .replace("postgres://", "postgresql://")
-    .replace("?__extra__=%7B%7D", "")
-)
-
-DWH_DB_DSN = (
-    Connection.get_connection_from_secrets("postgres_dwh")
-    .get_uri()
-    .replace("postgres://", "postgresql://")
-    .replace("?__extra__=%7B%7D", "")
-)
-
-api = create_engine(API_DB_DSN)
-dwh = create_engine(DWH_DB_DSN)
 
 
 with DAG(
     dag_id="copy_lecturer_rating",
     start_date=datetime(2025, 8, 23),
-    schedule=Dataset("DWH_RATING.lecturer"),
+    schedule="@daily",
     catchup=False,
     tags=["dwh", "api", "rating", "copy"],
     default_args={
@@ -46,17 +30,7 @@ with DAG(
         "on_failure_callback": partial(alert_message, chat_id=int(Variable.get("TG_CHAT_DWH"))),
     },
 ):
-    logging.info("Перенос рейтинга преподавателя из DWH_RATING в rating_api")
-    with api.connect() as api_conn, dwh.connect() as dwh_conn:
-        data = pd.read_sql_query(
-            f'SELECT api_id, mark_weighted, mark_kindness_weighted, mark_clarity_weighted, mark_freebie_weighted, rank '
-            f'FROM "DWH_RATING".lecturer as lect '
-            f'WHERE lect.valid_to_dt is NULL and lect.valid_from_dt = ('
-            f'SELECT MAX(valid_from_dt) FROM "DWH_RATING".lecturer)',
-            dwh_conn,
-            'api_id',
-        )
+    tg_task = send_telegram_message(int(Variable.get("TG_CHAT_DWH")))
+    copy = copy_rating_to_rating_api()
 
-        api_conn.execute('TRUNCATE TABLE "api_rating".lecturer_rating;')
-        data.to_sql(name="api_rating.lecturer_rating", con=api_conn, if_exists="append")
-        logging.info("Данные рейтинга преподавателей были скопированы с dwh в rating_api")
+    copy >> tg_task
