216818b019a361367eece4e77c91d6e3a1c84d7e
yaroslav <yaroslavsavateikin@yandex.ru>
2025-11-10 16:33:44 +0300

    additional_data json type check

--- dags/ods/marketing/printer_bots_actions.sql
+++ dags/ods/marketing/printer_bots_actions.sql
@@ -25,7 +25,8 @@ FROM
         SELECT value AS elem
         FROM jsonb_array_elements(
             CASE 
-                WHEN additional_data = '' THEN jsonb_build_array('{}'::jsonb)
+                WHEN additional_data IS NULL OR additional_data = '' THEN jsonb_build_array('{}'::jsonb)
+                WHEN additional_data::jsonb IS NULL THEN jsonb_build_array('{}'::jsonb)
                 WHEN jsonb_typeof(additional_data::jsonb) = 'array' THEN additional_data::jsonb
                 ELSE jsonb_build_array(additional_data::jsonb)
             END
