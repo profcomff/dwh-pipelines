7e3d45e24ad23015688f1c9e712592285c2b2d66
yaroslav <yaroslavsavateikin@yandex.ru>
2025-11-28 10:20:20 +0300

    jsonb_valid usage

--- dags/ods/marketing/printer_bots_actions.sql
+++ dags/ods/marketing/printer_bots_actions.sql
@@ -28,7 +28,7 @@ FROM
                 WHEN additional_data IS NULL OR additional_data = '' THEN jsonb_build_array('{}'::jsonb)
                 WHEN NOT jsonb_valid(additional_data) THEN jsonb_build_array('{}'::jsonb)
                 WHEN jsonb_typeof(additional_data::jsonb) = 'array' THEN additional_data::jsonb
-                ELSE jsonb_build_array(additional_data::jsonb)
+                ELSE jsonb_build_array(additional_data::jsonb) 
             END
         )
     ) AS expanded
